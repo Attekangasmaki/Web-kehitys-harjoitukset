22ea2777ddae0f69b1593801f4b23d75d4def672
Attekangasmaki <atte.kangasmaki@metropolia.fi>
2025-01-22 14:26:39 +0200

    Added features

--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use('/sivusto', express.static('public'));
+
+//middleware, joka lukee json-datan POST-pyyntöjen rungosta (body)
+app.use(express.json());
+
+app.get('/api/', (req, res) => {
   res.send('Welcome to my REST API!');
 });
 
-app.post('/', (req, res) => {
-  res.send('Welcome to my REST API! (post)');
+app.get('/api/sum/', (req, res) => {
+  console.log('req.params');
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2});
+});
+
+app.get('/api/sum/:num1/:num2', (req, res) => {
+  console.log('req.params');
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+  res.json({
+    num1,
+    num2,
+    sum: num1 + num2});
+});
+
+app.post('/api/moro', (req, res) => {
+  console.log(req.body);
+  res.send({reply: 'No moro ' + req.body.sender});
 });
 
 app.listen(port, hostname, () => {
