f0c1820160b31ea808fa470d54b5ecf334596828
Attekangasmaki <atte.kangasmaki@metropolia.fi>
2025-01-22 13:09:38 +0200

    yoyoyo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
-server.listen(port, hostname, () => {
+app.post('/', (req, res) => {
+  res.send('Welcome to my REST API! (post)');
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
